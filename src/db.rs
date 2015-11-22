97c2d718e186d4c3fa9e287607b51a29634525af
Matt Jones <jonesmr@gmail.com>
2015-11-22 16:38:33 -0500

    support for multiple records

--- src/db.rs
+++ src/db.rs
@@ -7,6 +7,7 @@ pub struct Db {
 }
 
 const VAL_WIDTH: usize = 8;
+const REC_WIDTH: usize = 2 * VAL_WIDTH;
 
 fn load(buf: &[u8]) -> u64 {
     assert_eq!(VAL_WIDTH, buf.len());
@@ -26,32 +27,49 @@ fn store(n: u64, buf: &mut [u8]) {
     }
 }
 
+// Stupid data layout
+// First 8 bytes: count of records
+// Each record is 16 bytes
+
 impl Db {
     pub fn new(storage: Box<Device>) -> Db {
         return Db{storage: storage}
     }
 
     pub fn record(&mut self, ts: u64, val: u64) -> io::Result<()> {
-//        try!(self.storage.read(0, VAL_WIDTH));
+        let mut rec_count_buf : [u8; VAL_WIDTH] = [0; VAL_WIDTH];
+        try!(self.storage.read(0, VAL_WIDTH as u64, &mut rec_count_buf));
+        let rec_count = load(&rec_count_buf);
 
-        let mut rec : [u8; 2 * VAL_WIDTH] = [0; 2 * VAL_WIDTH];
+        let offset = VAL_WIDTH as u64 + rec_count * REC_WIDTH as u64;
+        let mut rec : [u8; REC_WIDTH] = [0; REC_WIDTH];
         store(ts, &mut rec[0..VAL_WIDTH]);
         store(val, &mut rec[VAL_WIDTH..(2*VAL_WIDTH)]);
-        try!(self.storage.write(0, &rec));
+        try!(self.storage.write(offset, &rec));
+
+        store(rec_count + 1, &mut rec_count_buf);
+        try!(self.storage.write(0, &rec_count_buf));
+        
         return Ok(());
     }
 
     pub fn lookup(&mut self, ts: u64) -> io::Result<u64> {
-        let mut rec : [u8; 2 * VAL_WIDTH] = [0; 2 * VAL_WIDTH];
-        try!(self.storage.read(0, (2 * VAL_WIDTH) as u64, &mut rec));
-        let disk_ts = load(&rec[0..VAL_WIDTH]);
-        if disk_ts != ts {
-            return Err(io::Error::new(io::ErrorKind::Other, "mismatched ts"));
+        let mut rec_count_buf : [u8; VAL_WIDTH] = [0; VAL_WIDTH];
+        try!(self.storage.read(0, VAL_WIDTH as u64, &mut rec_count_buf));
+        let rec_count = load(&rec_count_buf);
+
+        let mut rec : [u8; REC_WIDTH] = [0; REC_WIDTH];
+        for i in 0..rec_count {
+            let offset = VAL_WIDTH as u64 + i * REC_WIDTH as u64;
+            try!(self.storage.read(offset, (REC_WIDTH) as u64, &mut rec));
+            let disk_ts = load(&rec[0..VAL_WIDTH]);
+            if disk_ts == ts {
+                let disk_val = load(&rec[VAL_WIDTH..(REC_WIDTH)]);
+                return Ok(disk_val);
+            }
         }
 
-        let disk_val = load(&rec[VAL_WIDTH..(2 * VAL_WIDTH)]);
-
-        return Ok(disk_val);
+        return Err(io::Error::new(io::ErrorKind::Other, "mismatched ts"));
     }
 }
 
@@ -84,6 +102,8 @@ mod test {
     fn db_test() {
         let mut db = Db::new(Box::new(InMemoryDevice::new(10240, 4)));
         db.record(1234567890, 257).unwrap();
+        db.record(1111111111, 1).unwrap();
         assert_eq!(257, db.lookup(1234567890).unwrap());
+        assert_eq!(1,   db.lookup(1111111111).unwrap());
     }
 }
