9d41b5111e4de471b98db8a3a81533da47e19200
Matt Jones <jonesmr@gmail.com>
2015-11-22 21:56:39 -0500

    refactoring, but not clearly better

--- src/db.rs
+++ src/db.rs
@@ -1,5 +1,3 @@
-extern crate time;
-
 use std::io;
 
 use block_storage::Device;
@@ -29,32 +27,16 @@ fn store(n: u64, buf: &mut [u8]) {
     }
 }
 
-fn store_timespec(t: time::Timespec, buf: &mut [u8]) {
-    assert_eq!(VAL_WIDTH, buf.len());
-
-    store(t.nsec as u64 + ((t.sec as u64) << 32), buf);
-}
-
-fn load_timespec(buf: &[u8]) -> time::Timespec {
-    assert_eq!(VAL_WIDTH, buf.len());
-
-    let modulus = 1 << 32;
-    let packed = load(buf);
-    let nsec = (packed % modulus) as i32;
-    let sec = (packed >> 32) as i64;
-
-    return time::Timespec::new(sec, nsec);
-}
-
-struct Rec {
-    timestamp: time::Timespec,
+#[derive(Debug,PartialEq)]
+pub struct Rec {
+    timestamp: u64,
     value: u64,
 }
 
-fn store_rec(rec: Rec, buf: &mut [u8]) {
+fn store_rec(rec: &Rec, buf: &mut [u8]) {
     assert_eq!(2 * VAL_WIDTH, buf.len());
 
-    store_timespec(rec.timestamp, &mut buf[0..VAL_WIDTH]);
+    store(rec.timestamp, &mut buf[0..VAL_WIDTH]);
     store(rec.value, &mut buf[VAL_WIDTH..(2*VAL_WIDTH)]);
 }
 
@@ -62,7 +44,7 @@ fn load_rec(buf: &[u8]) -> Rec {
     assert_eq!(2 * VAL_WIDTH, buf.len());
     
     let value = load(&buf[VAL_WIDTH..(2*VAL_WIDTH)]);
-    let timestamp = load_timespec(&buf[0..VAL_WIDTH]);
+    let timestamp = load(&buf[0..VAL_WIDTH]);
 
     return Rec{timestamp: timestamp, value: value};
 }
@@ -80,14 +62,13 @@ impl Db {
         return Db{storage: storage}
     }
 
-    pub fn record(&mut self, ts: u64, val: u64) -> io::Result<()> {
+    pub fn record(&mut self, rec: &Rec) -> io::Result<()> {
         let rec_count = try!(self.num_records());
 
         let offset = record_offset(rec_count);
-        let mut rec : [u8; REC_WIDTH] = [0; REC_WIDTH];
-        store(ts, &mut rec[0..VAL_WIDTH]);
-        store(val, &mut rec[VAL_WIDTH..(2*VAL_WIDTH)]);
-        try!(self.storage.write(offset, &rec));
+        let mut buf : [u8; REC_WIDTH] = [0; REC_WIDTH];
+        store_rec(&rec, &mut buf);
+        try!(self.storage.write(offset, &buf));
         try!(self.set_record_count(rec_count + 1));
         
         return Ok(());
@@ -96,14 +77,13 @@ impl Db {
     pub fn lookup(&mut self, ts: u64) -> io::Result<u64> {
         let rec_count = try!(self.num_records());
 
-        let mut rec : [u8; REC_WIDTH] = [0; REC_WIDTH];
+        let mut buf : [u8; REC_WIDTH] = [0; REC_WIDTH];
         for i in 0..rec_count {
             let offset = record_offset(i);
-            try!(self.storage.read(offset, (REC_WIDTH) as u64, &mut rec));
-            let disk_ts = load(&rec[0..VAL_WIDTH]);
-            if disk_ts == ts {
-                let disk_val = load(&rec[VAL_WIDTH..(REC_WIDTH)]);
-                return Ok(disk_val);
+            try!(self.storage.read(offset, (REC_WIDTH) as u64, &mut buf));
+            let rec = load_rec(&buf);
+            if rec.timestamp == ts {
+                return Ok(rec.value);
             }
         }
 
@@ -126,12 +106,20 @@ impl Db {
 
 #[cfg(test)]
 mod test {
+    extern crate time;
+    
     use block_storage::InMemoryDevice;
     use std::io::ErrorKind;
     
     use super::Db;
+    use super::Rec;
+    
     use super::load;
+    use super::load_rec;
     use super::store;
+    use super::store_rec;
+
+    use super::REC_WIDTH;
     use super::VAL_WIDTH;
 
     fn round_trip(n: u64) -> u64{
@@ -139,6 +127,13 @@ mod test {
 
         store(n, &mut buf);
         return load(&buf);
+    }
+
+    fn round_trip_rec(rec: Rec) {
+        let mut buf : [u8; REC_WIDTH] = [0; REC_WIDTH];
+        store_rec(&rec, &mut buf);
+        let rec2 = load_rec(&buf);
+        assert_eq!(rec, rec2);
     }
 
     #[test]
@@ -149,13 +144,23 @@ mod test {
         assert_eq!(256, round_trip(256));
         assert_eq!(1234567890, round_trip(1234567890));
         assert_eq!(u64::max_value(), round_trip(u64::max_value()));
+
+    }
+
+    #[test]
+    fn encode_decode_rec() {
+        round_trip_rec(Rec{
+            timestamp: 1234567890,
+            value: 257,
+        });
     }
 
     #[test]
     fn db_test() {
         let mut db = Db::new(Box::new(InMemoryDevice::new(10240, 4)));
-        db.record(1234567890, 257).unwrap();
-        db.record(1111111111, 1).unwrap();
+        db.record(&Rec{timestamp: 1234567890, value: 257}).unwrap();
+        db.record(&Rec{timestamp: 1111111111, value: 1}).unwrap();
+
         assert_eq!(257, db.lookup(1234567890).unwrap());
         assert_eq!(1,   db.lookup(1111111111).unwrap());
         assert_eq!(ErrorKind::NotFound,
